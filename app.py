1260e713da7486ed42aa8555aa130b6033cc3e37
MorradABattah <morradbattah1995@gmail.com>
2023-07-22 23:45:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,43 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    # rest of your code...
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            logging.info('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            logging.info('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            logging.info('File saved successfully')
+            csv_to_txt(filepath)
+            return redirect('/downloads/' + filename.rsplit('.', 1)[0] + '.txt')
+    return '''
+    <!doctype html>
+    <title>Upload CSV file</title>
+    <h1>Upload CSV file</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+def csv_to_txt(filepath):
+    try:
+        df = pd.read_csv(filepath)
+        txt_filename = filepath.rsplit('.', 1)[0] + '.txt'
+        df.to_csv(txt_filename, sep='\t', index=False)
+        logging.info('File converted to txt')
+    except Exception as e:
+        logging.error('Failed to convert file: ' + str(e))
+
+@app.route('/downloads/<filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == "__main__":
+    app.run(port=5000)
